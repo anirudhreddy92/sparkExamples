5a5d69ad82759b9d556082956f27694dec3656cc
anirudhreddy92 <anirudh@Anirudh_reddy>
2016-08-03 16:25:23 -0400

    first commit

---
+++ src/main/java/Spark/Example/StatSummary/Example1.java
@@ -0,0 +1,15 @@
+package Spark.Example.StatSummary;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.StatCounter;
+@SuppressWarnings("unused")
+
+public class Example1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Spark/Example/StatSummary/VimeoExample.java
@@ -0,0 +1,10 @@
+package Spark.Example.StatSummary;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+@SuppressWarnings("unused")
+public class VimeoExample {
+
+}
